d3faefb557aa017cac04141dee7de31b05157596
Павло <pashkevuchpasha@gmail.com>
2024-02-28 23:51:28 +0200

    something

--- moovies/moviepage/models.py
+++ moovies/moviepage/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-# class Producer(models.Model):
-#     name =  models.TextField()
-#
-# class Actor(models.Modle):
-#     name = models.TextField()
-#
-# class Moovie(models.Model):
-#     name = models.CharField()
-#     release_year = models.DateField()
-#     producer = models.ForeignKey(Producer)
-#     actor = models.ForeignKey(Actor)
+
+class Producer(models.Model):
+    name = models.CharField(max_length=30)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=30)
+
+class Moovie(models.Model):
+    name = models.CharField(max_length=30)
+    release_year = models.DateField()
+    producer = models.ForeignKey(Producer, on_delete=models.PROTECT)
+    actor = models.ForeignKey(Actor, on_delete=models.PROTECT)
--- moovies/moviepage/views.py
+++ moovies/moviepage/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-# import requests
+import requests
+from .models import Moovie
+from rest_framework import generics
+
+class MoovieAPIView(generics.ListAPIView):
+    queryset = Moovie.objects.all()
 
 class MoovieList(ListView):
-    template_name = 'mooviepage/film_list.html'
-    model = None
+    template_name = 'moviepage/film_list.html'
+    model = Moovie
 
     def get(self, *args, **kwargs):
         return super(MoovieList, self).get(*args, **kwargs)
 
     def get_context_data(self, *args, **kwargs):
-        print('Hello World!!!')
-        print(kwargs)
-        context = {}
+        context = kwargs
+
+        r = requests.get('https://www.omdbapi.com/?apikey=8f357ea6&t=Batman').json()
+        print('here')
+        print(r)
+        # print(r.title)
+
+        # film = Moovie()
+        # film.name =
         return context
