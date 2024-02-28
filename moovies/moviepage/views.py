99ea776fe6d9c5d02032bf824e2777a3814b594b
pkravets@loe.lviv.ua <pkravets@loe.lviv.ua>
2024-02-28 16:46:23 +0200

    requirenments

--- moovies/moviepage/views.py
+++ moovies/moviepage/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-
+import requests
 
 class MoovieList(ListView):
     template_name = 'mooviepage/film_list.html'
     model = None
+
+    def get(self, *args, **kwargs):
+        return super(MoovieList, self).get(*args, **kwargs)
 
     def get_context_data(self, *args, **kwargs):
         print('Hello World!!!')
